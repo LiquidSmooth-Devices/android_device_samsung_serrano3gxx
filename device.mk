cbc8c4bf5604f9412fde9b5040757318dae41469
Sean Hoyt <seanhoyt963@gmail.com>
2014-06-16 10:49:17 -0500

    Revert "Add OTA ID"
    This reverts commit 9b9b441e83b53d6a2b1e9805691ec8e3c7f7fd09.

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@
 # Inherit from serrano-common
 $(call inherit-product, device/samsung/serrano-common/serrano-common.mk)
 
-# OTA ID
-otaupdater.otaid=serrano3gxx
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
